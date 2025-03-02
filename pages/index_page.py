99aa424096c2607c17507f4750b2d003c9fa42ca
Andrei Konovalov <a.konovaloff13@yandex.com>
2025-03-03 00:37:23 +0400

    Added test_check_time_estimation

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,8 +1,28 @@
 from playwright.sync_api import Page
 import config
-
+import time
 
 class IndexPage:
 
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
+
+    def get_page_title(self, page: Page):
+        return page.title()
+
+    def __set_station__(self, page:Page, text:str, locator:str):
+        station_from_element = page.locator(locator)
+        station_from_element.click()
+        station_from_element.fill(text)
+        element = page.locator("span", has_text=text)
+        element.click()
+
+    def set_station_from(self, page: Page, text: str):
+        self.__set_station__(page, text, "//*[@placeholder='Откуда']")
+
+
+    def set_station_to(self, page: Page, text:str):
+        self.__set_station__(page, text,"//*[@placeholder='Куда']")
+
+    def get_estimation_time(self, page: Page) -> str:
+        return page.locator("//span[@class = 'route-list-item__time']").text_content()
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -5,6 +5,15 @@ import time
 
 class TestFooter:
 
-    def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
+    def test_main_page_title(self, page):
+        pages.index_page.open_index_page(page)
+        assert pages.index_page.get_page_title(page) == "Схема метро Москвы — Яндекс.Метро"
+
+    def test_check_time_estimation(self, page):
         pages.index_page.open_index_page(page)
         time.sleep(5)
+        pages.index_page.set_station_from(page, "Красные Ворота")
+        pages.index_page.set_station_to(page, "Черкизовская")
+        assert pages.index_page.get_estimation_time(page) == "≈\xa013 мин."
+        time.sleep(3)
+
